bf74eecd639051996b207a312ccdabd2b83157d2
전재욱 <wjswodnr100@gmail.com>
2024-01-18 21:28:07 +0900

    server script는 다시 뺌

--- server_script/db_script/reset_played_count_of_week.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Set the path to the .pgpass file
-PGPASSFILE="/home/ubuntu/.pgpass"
-
-# Run the SQL query using the credentials from .pgpass
-psql -U quizbot -h localhost -d quizbot3 -c "UPDATE tb_quiz_info SET played_count_of_week = 0;"
-
