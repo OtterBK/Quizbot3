a03c104ef489b4d31dc3b4f6c01bacc66323aff9
Jeon Jae Uk <wjswodnr100@gmail.com>
2024-07-16 10:48:45 +0900

    cron 등록 대상 변경

--- auto_script/setup_quizbot3.sh
+++ auto_script/setup_quizbot3.sh
@@ -8,7 +8,7 @@ usage() {
 
 # Variables
 INSTALL_PATH=""
-NODE_VERSION="16"
+NODE_VERSION="18"
 BACKUP_FILE=""
 REGISTER_CRON=false
 SWAP_MEM=""
@@ -16,26 +16,26 @@ SWAP_MEM=""
 # Process parameters
 while [ "$#" -gt 0 ]; do
     case "$1" in
-		--install-path=*)
-			INSTALL_PATH="${1#*=}"
-			shift 1
-			;;
+        --install-path=*)
+            INSTALL_PATH="${1#*=}"
+            shift 1
+            ;;
         --node=*)
             NODE_VERSION="${1#*=}"
             shift 1
             ;;
-		--cron)
+        --cron)
             REGISTER_CRON=true
             shift 1
             ;;
-		--dump=*)
-			BACKUP_FILE="${1#*=}"
+        --dump=*)
+            BACKUP_FILE="${1#*=}"
             shift 1
             ;;
-		--swap=*)
-		    SWAP_MEM="${1#*=}"
-		    shift 1
-		    ;;
+        --swap=*)
+            SWAP_MEM="${1#*=}"
+            shift 1
+            ;;
         --help)
             usage
             ;;
@@ -47,15 +47,15 @@ done
 
 # Check if --install-path option is provided
 if [ -n "$INSTALL_PATH" ]; then
-	# Check if INSTALL_PATH is a valid directory
-	if [ ! -d "$INSTALL_PATH" ]; then
-		echo "Error: Invalid install path. Please provide a valid directory."
-		exit 1
-	fi
+    # Check if INSTALL_PATH is a valid directory
+    if [ ! -d "$INSTALL_PATH" ]; then
+        echo "Error: Invalid install path. Please provide a valid directory."
+        exit 1
+    fi
     echo "Install path set to: $INSTALL_PATH"
 else
-	echo "The --install-path parameter is required"
-	exit 1
+    echo "The --install-path parameter is required"
+    exit 1
 fi
 
 # Check if --cron option is provided
@@ -73,11 +73,11 @@ fi
 
 # Check if --dump option is provided
 if [ -n "$BACKUP_FILE" ]; then
-	# Check if INSTALL_PATH is a valid directory
-	if [ ! -f "$BACKUP_FILE" ]; then
-		echo "Error: Invalid BACKUP_FILE path. Please provide a valid file."
-		exit 1
-	fi
+    # Check if INSTALL_PATH is a valid directory
+    if [ ! -f "$BACKUP_FILE" ]; then
+        echo "Error: Invalid BACKUP_FILE path. Please provide a valid file."
+        exit 1
+    fi
     echo "Running with --dump option. Restoring database from backup file: $BACKUP_FILE"
 fi
 
@@ -122,9 +122,9 @@ sudo apt install nodejs -y
 # If --dump option is provided, restore database from the specified backup file
 if [ -n "$BACKUP_FILE" ]; then
     print_emphasized "Restoring database from the specified backup file: $BACKUP_FILE"
-	cp $BACKUP_FILE /tmp
+    cp $BACKUP_FILE /tmp
     sudo -u postgres psql -d quizbot3 -f "/tmp/$(basename "$BACKUP_FILE")"
-	rm "/tmp/$(basename "$BACKUP_FILE")"
+    rm "/tmp/$(basename "$BACKUP_FILE")"
 fi
 
 #print_emphasized "Modifying pg_hba.conf to use md5 for all local peers..."
@@ -144,21 +144,21 @@ print_emphasized "Quizbot3 has been Installed!!!!!!!!!!!!!!!!!!!!"
 
 # Setting Cron Scheduler
 if [ "$REGISTER_CRON" = true ]; then
-	print_emphasized "Installing Cron..."
+    print_emphasized "Installing Cron..."
     sudo timedatectl set-timezone Asia/Seoul
-	sudo apt install cron -y
-	sudo service cron start
+    sudo apt install cron -y
+    sudo service cron start
 
-	print_emphasized "Executing Register cron script"
-	SCRIPT_PATH="$(dirname "$(readlink -f "$0")")"
-    (crontab -l 2>/dev/null; echo "CRON_TZ=Asia/Seoul") | crontab -
-	(crontab -l 2>/dev/null; echo "0 * * * * sudo sh $SCRIPT_PATH/server_script/drop_ffmpeg.sh") | crontab -
-	(crontab -l 2>/dev/null; echo "0 9,21 * * * sudo sh $SCRIPT_PATH/server_script/quizbot_stop.sh") | crontab -
-	(crontab -l 2>/dev/null; echo "1 9,21 * * * sudo sh $SCRIPT_PATH/server_script/quizbot_start.sh") | crontab -
+    print_emphasized "Executing Register cron script"
+    SCRIPT_PATH="$(dirname "$(readlink -f "$0")")"
+    (crontab -l -u ubuntu 2>/dev/null; echo "CRON_TZ=Asia/Seoul") | sudo crontab -u ubuntu -
+    (crontab -l -u ubuntu 2>/dev/null; echo "0 * * * * sudo sh $SCRIPT_PATH/server_script/drop_ffmpeg.sh") | sudo crontab -u ubuntu -
+    (crontab -l -u ubuntu 2>/dev/null; echo "0 9,21 * * * sh $SCRIPT_PATH/server_script/quizbot_stop.sh") | sudo crontab -u ubuntu -
+    (crontab -l -u ubuntu 2>/dev/null; echo "1 9,21 * * * sh $SCRIPT_PATH/server_script/quizbot_start.sh") | sudo crontab -u ubuntu -
 
-	(crontab -l 2>/dev/null; echo "0 0 * * 1 sudo sh $SCRIPT_PATH/db_script/reset_played_count_of_week.sh") | crontab -
-	(crontab -l 2>/dev/null; echo "0 8,20 * * * sudo sh $SCRIPT_PATH/db_script/backup_script.sh") | crontab -
- 	(crontab -l 2>/dev/null; echo "0 */3 * * * sudo sync && echo 3 > /proc/sys/vm/drop_caches") | crontab -
+    (crontab -l -u ubuntu 2>/dev/null; echo "0 0 * * 1 sudo sh $SCRIPT_PATH/db_script/reset_played_count_of_week.sh") | sudo crontab -u ubuntu -
+    (crontab -l -u ubuntu 2>/dev/null; echo "0 8,20 * * * sudo sh $SCRIPT_PATH/db_script/backup_script.sh") | sudo crontab -u ubuntu -
+    (crontab -l -u ubuntu 2>/dev/null; echo "0 */3 * * * sudo sync && echo 3 > /proc/sys/vm/drop_caches") | sudo crontab -u ubuntu -
 fi
 
 # Setting Swap Memory
@@ -170,22 +170,22 @@ if [ -n "$SWAP_MEM" ]; then
 
     # Check if swapfile already exists
     if [ -e "$SWAPFILE" ]; then
-    	 echo "Swapfile already exists. Exiting."
+        echo "Swapfile already exists. Exiting."
     else
 
-    	# Create a swapfile
-    	sudo fallocate -l $SWAPSIZE $SWAPFILE
-	sudo chmod 600 $SWAPFILE
-    	sudo mkswap $SWAPFILE
-    	sudo swapon $SWAPFILE
+        # Create a swapfile
+        sudo fallocate -l $SWAPSIZE $SWAPFILE
+        sudo chmod 600 $SWAPFILE
+        sudo mkswap $SWAPFILE
+        sudo swapon $SWAPFILE
 
-    	# Add the swapfile entry to /etc/fstab to make it persistent across reboots
-    	echo "$SWAPFILE none swap sw 0 0" | sudo tee -a /etc/fstab
+        # Add the swapfile entry to /etc/fstab to make it persistent across reboots
+        echo "$SWAPFILE none swap sw 0 0" | sudo tee -a /etc/fstab
 
-    	# Display the swap information
-    	echo "Swapfile created and activated:"
-    	sudo swapon --show
+        # Display the swap information
+        echo "Swapfile created and activated:"
+        sudo swapon --show
     fi
 fi    
 
-print_emphasized "Quizbot3 Auto Setup Finised! :)"
+print_emphasized "Quizbot3 Auto Setup Finished! :)"
