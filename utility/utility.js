f6a374f4f2c00e1740eb016580d9d2faab9eb483
전재욱 <wjswodnr100@gmail.com>
2024-08-22 20:36:00 +0900

    short 비디오 id 검증되도록 변경

--- utility/utility.js
+++ utility/utility.js
@@ -433,8 +433,7 @@ exports.getIPv4Address = () => {
 };
 
 exports.extractYoutubeVideoID = (url) => {
-  const regex = /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:watch\?v=|embed\/|v\/|.+\/.+\/)|youtu\.be\/)([a-zA-Z0-9_-]{11})/;
+  const regex = /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:watch\?v=|embed\/|v\/|shorts\/|.+\/.+\/)|youtu\.be\/)([a-zA-Z0-9_-]{11})/;
   const match = url.match(regex);
-
   return match ? match[1] : undefined;
 };
